8167e92a448b6003c5c98e78c031b0f0c6a0c6ea
apnarozgar <apnarozgarapp@gmail.com>
2018-04-19 09:47:31 +0530

    ram

--- project/project/settings.py
+++ project/project/settings.py
@@ -108,4 +108,4 @@ AUTH_PROFILE_MODULE = 'worker.Profile'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/login/static/login/'
